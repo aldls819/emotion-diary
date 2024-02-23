b34d5c40a85b7ae78989354a646058651179ec4e
aldls819 <aldls819@gmail.com>
2024-02-23 18:10:42 +0900

    :sparkles: 라우터 응용
    라우터 응용

--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Diary = () => {
+  const { id } = useParams();
+
   return (
     <div>
       <h1>여기는 Diary</h1>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+  const [searchParmas, setSearchParams] = useSearchParams();
+  const id = searchParmas.get("id");
+  const mode = searchParmas.get("mode");
+
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>여기는 Edit</h1>
+      <button onClick={() => navigate("/home")}>홈으로</button>
     </div>
   );
 };
