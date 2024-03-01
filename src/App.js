890c452dcfb52406d789eb1488fdd24f396ca4e2
aldls819 <aldls819@gmail.com>
2024-03-01 18:34:19 +0900

    :sparkles: 로컬스토리지 추가
    로컬 스토리지 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from "react";
+import React, { useEffect, useReducer, useRef } from "react";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -32,51 +32,28 @@ const reducer = (state, action) => {
     default:
       return state;
   }
+  localStorage.setItem("diary", JSON.stringify(newState));
   return newState;
 };
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
-const dummyData = [
-  {
-    id: 1,
-    emotion: 1,
-    content: "오늘의 일기 1번",
-    date: 1708962028592,
-  },
-  {
-    id: 2,
-    emotion: 3,
-    content: "오늘의 일기 2번",
-    date: 1708962028593,
-  },
-  {
-    id: 3,
-    emotion: 2,
-    content: "오늘의 일기 3번",
-    date: 1708962028594,
-  },
-  {
-    id: 4,
-    emotion: 5,
-    content: "오늘의 일기 4번",
-    date: 1708962028595,
-  },
-  {
-    id: 5,
-    emotion: 4,
-    content: "오늘의 일기 5번",
-    date: 1708962028596,
-  },
-];
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, dummyData);
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
 
-  console.log(new Date().getTime());
+  useEffect(() => {
+    const localData = localStorage.getItem("diary");
+    if (localData) {
+      const diaryList = JSON.parse(localData).sort(
+        (a, b) => parseInt(b.id) - parseInt(a.id)
+      );
 
-  const dataId = useRef(6);
+      dispatch({ type: "INIT", data: diaryList });
+    }
+  }, []);
+
   //CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
